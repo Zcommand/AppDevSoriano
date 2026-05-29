d8ca93b60b7015a02ae242298e908d9df467a6ca
Zcommand <zumoshimo25@gmail.com>
2026-05-30 00:25:37 +0800

    try

---
+++ module4/practice.php
@@ -0,0 +1,66 @@
+<?php
+$pageTitle = "Home";
+
+$books = [
+    [
+        "title" => "spiderman",
+        "author" => "Marvel",
+        "year" => "2026",
+        "image" => ""
+    ],
+    [
+        "title" => "spiderman",
+        "author" => "Marvel",
+        "year" => "2026",
+        "image" => ""
+    ],
+    [
+        "title" => "spiderman",
+        "author" => "Marvel",
+        "year" => "2026",
+        "image" => ""
+    ]
+];
+
+usort($books, fn($a, $b) => strcmp($a["title"], $b["title"]));
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $pageTitle ?></title>
+</head>
+
+<body>
+    <div class = container>
+
+        <div class = header>
+            My books
+        </div>
+
+        <table>
+            <tr>
+                <th>title</th>
+                <th>author</th>
+                <th>year</th>
+                <th>image</th>
+            </tr>
+        
+        
+        <?php foreach ($books as $book): ?>
+            <tr>
+                <td><?= $book["title"]?></td>
+                <td><?= $book["author"]?></td>
+                <td><?= $book["year"]?></td>
+                <td><?= $book["image"]?></td>
+            </tr>
+        <?php endforeach; ?>
+        </table>
+    </div>
+</body>
+
+</html>
